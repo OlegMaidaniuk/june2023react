f87176df747133525b276d0c64bb46f55604d26f
OlegMaidaniuk <omaydanuk@gmail.com>
2023-10-24 12:13:08 +0300

    strict aut master del test.js clean App.css lesson1/1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {Simpsons} from "./components/Simpsons/Simpsons";
 
 function App() {
   return (
     <div className="App">
       <h2>lesson1ReactMaidaniukOV</h2>
+        <Simpsons/>
+        <Simpsons/>
+        <Simpsons/>
     </div>
   );
 }
